b8ec74403e64f1bb2cd14b3609d81a853a23b168
PORRADENOME <84040149+PORRADENOME@users.noreply.github.com>
2021-10-18 21:04:16 -0300

    alteração perfil

--- cliente/cliente/perfil_cliente.php
+++ cliente/cliente/perfil_cliente.php
@@ -2,7 +2,7 @@
 require "../configurações/segurança.php";
 try {
     require "../configurações/conexao.php";
-    var_dump($_SESSION);
+    // var_dump($_SESSION);
 
     $query = $conexao->PREPARE("SELECT * FROM cliente WHERE id_cliente=:id");
     $query->bindValue(":id", $_SESSION['id']);
@@ -15,7 +15,7 @@ try {
 
     $linhacliente = $query->fetchObject();
 
-    var_dump($linhacliente);
+    // var_dump($linhacliente);
 
 }catch(PDOException $exception){
     echo $exception->getMessage();
@@ -29,3 +29,16 @@ include ("../configurações/menu.php");
     <label for="nome_cliente">Nome</label>
     <input class="form-control" id="nome_cliente" type="text" name="nome_cliente" readonly value="<?php echo $linhacliente->nome_cliente;?>">
 </div>
+<div class="form-group">
+    <label for="email_cliente">Email</label>
+    <input class="form-control" id="email_cliente" type="text" name="email_cliente" readonly value="<?php echo $linhacliente->email_cliente;?>">
+</div>
+<div class="form-group">
+    <label for="cpf_cliente">CPF</label>
+    <input class="form-control" id="cpf_cliente" type="text" name="cpf_cliente" readonly value="<?php echo $linhacliente->cpf_cliente;?>">
+</div>
+<div class="form-group">
+    <label for="telefone_cliente">Telefone</label>
+    <input class="form-control" id="telefone_cliente" type="text" name="telefone_cliente" readonly value="<?php echo $linhacliente->telefone_cliente;?>">
+</div>
+
