f4a977cb467408d034f2d657024d8e9c09029bad
colthiagofachini <74021416+colthiagofachini@users.noreply.github.com>
2021-10-21 09:15:22 -0300

    Update menu.php

--- motorista/configurações/menu.php
+++ motorista/configurações/menu.php
@@ -10,16 +10,7 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="//https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Clientes</a>
                 <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="../cliente/perfil_cliente.php">Listagem</a>
-                </div>
-
-            </li>
-
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="//https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Orcamento</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="../orcamento/listagem_orcamento.php">Listagem_orcamento</a>
-                    <a class="dropdown-item" href="../orcamento/cadastro_orcamento.php">Cadastro de orcamento</a>
+                    <a class="dropdown-item" href="../motorista/perfil_motorista.php">Listagem</a>
                 </div>
 
             </li>
