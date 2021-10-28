b3b3df7fa205f2a99dac25b05efefeb649b6bb03
colthiagofachini <74021416+colthiagofachini@users.noreply.github.com>
2021-10-28 08:24:37 -0300

    avaliacao motorista

---
+++ motorista/avaliacao/inserir_avaliacao.php
@@ -0,0 +1,31 @@
+<?php
+require "../configurações/segurança.php";
+try {
+
+    include "../configurações/conexao.php";
+
+   /*
+    $query = $conexao->prepare("SELECT * FROM funcionario WHERE funcionario=:funcionario");
+    $query->bindValue(':funcionario',$_POST['funcionario']);
+    $query->execute();
+    if ($query->rowCount() == 1) {
+        retornaErro('funcionario ja em uso');
+    }
+   */
+
+
+    $query = $conexao->prepare("INSERT INTO avaliacao (texto_avaliacao,data_avaliacao ) VALUES (:texto_avaliacao,:data_avaliacao) ");
+    $query->bindValue(':texto_avaliacao',$_POST['texto_avaliacao']);
+    $query->bindValue(':data_avaliacao',$_POST['data_avaliacao']);
+
+    $query->execute();
+
+    if ($query->rowCount() == 1) {
+        retornaOK('Inserido com sucesso ');
+    } else {
+        retornaErro('Erro ao inserir');
+    }
+
+} catch (Exception $exception) {
+    retornaErro($exception->getMessage());
+}
---
+++ motorista/avaliacao/listagem_avaliacao.php
@@ -0,0 +1,84 @@
+<?php
+require "../configurações/segurança.php";
+try {
+require "../configurações/conexao.php";
+
+}catch(PDOException $exception){
+    echo $exception->getMessage();
+}
+
+include ("../configurações/bootstrap.php");
+include ("../configurações/menu.php");
+?>
+
+<link href="../js/jquery.bootgrid.css" rel="stylesheet" />
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Listagem - Avaliacao</h1>
+    <br>
+            <table id="grid-data" class="table table-condensed table-hover table striped">
+                <thead>
+                <tr>
+                    <th data-column-id="id_avaliacao">ID</th>
+                    <th data-column-id="texto_avaliacao" data-order="desc" data-sortable="true">Texto</th>
+                    <th data-column-id="data_avaliacao" data-sortable="true">Data</th>
+                    <th data-column-id="commands" data-formatter="commands" data-sortable="false"></th>
+
+                </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
+
+<script src="../js/jquery.bootgrid.js"></script>
+<script src="../js/jquery.bootgrid.fa.js"></script>
+
+
+<script>
+    var grid;
+    $(document). ready(function () {
+        grid=$("#grid-data").bootgrid({
+            ajax: true,
+            url: "bootgrid.php",
+            formatters: {
+                "commands": function(column, row)
+                {
+                    return "<button type=\"button\" class=\"btn btn-danger command-delete\" data-row-id=\"" + row.id_avaliacao + "\"><span class=\"fas fa-trash\"></span></button>";
+                }
+            }
+        }).on ("loaded.rs.jquery.bootgrid", function () {
+            grid.find(".command-edit").on("click", function (e) {
+                document.location='form_editar_funcionario.php?id=' + $(this).data("row-id");
+            }).end().find(".command-delete").on("click", function (e)
+            {
+                iziToastExcluir($(this).data("row-id"));
+
+            });
+
+        });
+
+    });
+    function excluir(id) {
+        $.post(
+            "excluir_avaliacao.php",
+            {id: id},
+            function (data) {
+                if (data.status == 0) {
+                    iziToast.error({
+                        message: data.mensagem
+                    });
+                } else {
+                    iziToast.success({
+                        message: data.mensagem
+                    });
+                    grid.bootgrid("reload");
+                }
+            },
+            "json"
+        );
+    }
+
+</script>
